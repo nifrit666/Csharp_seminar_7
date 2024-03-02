55d3cf31f6c2992ba93f478333cacc31190b1c70
«Andrey_Istomin» <an.istomin@gmail.com>
2024-03-03 00:33:21 +0500

    Решение HomeWork_2 (промежуточное)

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿/*
+Задача 2: 
+Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Пример: 
+m = 2, n = 3 => A(m,n) = 29
+*/
+
+
+Console.WriteLine("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Результат: {FunctionAccerman(m, n)}");
+
+int FunctionAccerman(int num1, int num2)
+{
+    if (num1 == 0)
+    {
+        return (num2 + 1);
+    }
+    else if (num2 == 0)
+    {
+        return FunctionAccerman(num1 - 1, 1);
+    }
+    else
+    {
+        return FunctionAccerman(num1 - 1, FunctionAccerman(num1, num2 - 1));
+    }
+}
