3687fed453af145a41860f0c8c1b6d60acfc2dfa
«Andrey_Istomin» <an.istomin@gmail.com>
2024-03-02 23:54:56 +0500

    Решение Task1 Моё и в классе

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N. 
+Пример: 
+N = 5  =>  1 2 3 4 5 
+
+
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+
+    NaturalNumbers(num - 1); // оно уменьшается здесь 
+    Console.Write($"{num} ");
+}
+
+*/
+
+// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+void Main()
+{
+    int N = ReadInt("Введите число: ");
+PrintNumbers(N);
+}
+
+void PrintNumbers(int number)
+{
+    if (number < 1) return;
+    PrintNumbers(number - 1);
+    System.Console.Write(number + " ");
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+Main();
