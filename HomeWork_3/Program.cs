5211f09a00a694dcb0ef98730378af3a4ed7e8c5
«Andrey_Istomin» <an.istomin@gmail.com>
2024-03-03 00:55:45 +0500

    Решение HomeWork_3

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿/*
+Задача 3: 
+Задайте произвольный массив. 
+Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.
+
+Пример: 
+[1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+*/
+
+
+int[] arr = { 1, 2, 5, 0, 10, 34 };
+PrintReverseArray(arr, 0); 
+
+void PrintReverseArray(int[] Array, int index)
+{
+    if (index > Array.Length - 1) 
+    {
+        return;
+    }
+
+    PrintReverseArray(Array, index + 1);
+    Console.Write(Array[index] + " ");
+}
